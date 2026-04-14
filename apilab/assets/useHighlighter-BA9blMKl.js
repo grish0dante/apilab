8d52c1de9fae3cfbc83e47c14c2f2d4dbd134ba1
grish0dante <danil.grishanov.0@gmail.com>
2026-04-14 11:27:15 +0300

    Updates

---
+++ apilab/assets/useHighlighter-BA9blMKl.js
@@ -0,0 +1,2 @@
+import{d as t,r}from"./entry.client-DE9gSxWr.js";const g=()=>{const{syntaxHighlighting:i}=t().options;if(!i)throw new Error("Syntax highlighting not configured. Provide highlighterPromise in syntaxHighlighting options.");return r.use(i.highlighterPromise)};export{g as u};
+//# sourceMappingURL=useHighlighter-BA9blMKl.js.map
