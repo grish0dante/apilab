c1403af915d7343f1aee9acebd81347db25e821d
grish0dante <danil.grishanov.0@gmail.com>
2026-04-14 14:58:06 +0300

    Updates

--- assets/index-CM0S4H_i.js
+++
@@ -1,2 +0,0 @@
-import{r as u}from"./entry.client-BMigqD-2.js";function o(r){const e=u.useRef({value:r,previous:r});return u.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}export{o as u};
-//# sourceMappingURL=index-CM0S4H_i.js.map
--- assets/installation-9WxzNVN8.js
+++
@@ -1,5 +0,0 @@
-import{u as o,j as n}from"./entry.client-BMigqD-2.js";const a="npx create-zudoku@latest my-docs",r=[],i={title:"Installation",lastModifiedTime:"2026-04-14T07:57:34.000Z"},l="pages/installation.mdx";function s(e){const t={p:"p",...o(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(t.p,{children:"npx create-zudoku@latest my-docs"}),`
-`,n.jsx(t.p,{children:"cd my-docs"}),`
-`,n.jsx(t.p,{children:"npm install"}),`
-`,n.jsx(t.p,{children:"npm run dev"})]})}function d(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(s,{...e})}):s(e)}export{l as __filepath,d as default,a as excerpt,i as frontmatter,r as tableOfContents};
-//# sourceMappingURL=installation-9WxzNVN8.js.map
