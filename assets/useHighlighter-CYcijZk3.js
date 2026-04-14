1a7b9231991b6ae55a14cc5791adb3227294d8a9
grish0dante <danil.grishanov.0@gmail.com>
2026-04-14 11:47:10 +0300

    Updates

---
+++ assets/useHighlighter-CYcijZk3.js
@@ -0,0 +1,2 @@
+import{d as t,r}from"./entry.client-Dud-QfD4.js";const g=()=>{const{syntaxHighlighting:i}=t().options;if(!i)throw new Error("Syntax highlighting not configured. Provide highlighterPromise in syntaxHighlighting options.");return r.use(i.highlighterPromise)};export{g as u};
+//# sourceMappingURL=useHighlighter-CYcijZk3.js.map
---
+++ assets/useWarmupSchema-DRhsskS7.js
@@ -0,0 +1,8 @@
+import{R as u,U as n,p as t,X as s}from"./entry.client-Dud-QfD4.js";const r=s(`
+  query SchemaWarmup($input: JSON!, $type: SchemaType!) {
+    schema(input: $input, type: $type) {
+      openapi
+    }
+  }
+`),y=()=>{const{input:e,type:p}=u(),a=n(r,{input:e,type:p});t({...a,enabled:typeof window<"u",notifyOnChangeProps:[]})};export{y as u};
+//# sourceMappingURL=useWarmupSchema-DRhsskS7.js.map
